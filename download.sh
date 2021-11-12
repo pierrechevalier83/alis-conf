6d5262e8326607f7dab9aad386d729f24f713671
Pierre Viseu Chevalier <pierrechevalier83@gmail.com>
2021-11-12 22:56:01 +0000

    Download post_install files

--- download.sh
+++ download.sh
@@ -13,6 +13,10 @@ rm -f alis_custom.chromebook.conf
 rm -f alis_custom.T14.conf
 rm -f alis_package_custom.sh
 rm -f configure.sh
+rm -f post_install.sh
+rm -f dconf.settings
+rm -f dconf.chromebook.settings
+rm -f dconf.T14.settings
 
 curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis-conf/main/alis_custom.conf
 curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis-conf/main/alis_custom.vm.conf
@@ -20,6 +24,10 @@ curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis-conf/main/alis_custo
 curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis-conf/main/alis_custom.T14.conf
 curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis-conf/main/alis_packages_custom.conf
 curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis-conf/main/configure.sh
+curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis-conf/main/post_install.sh
+curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis-conf/main/templates/dconf.settings
+curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis-conf/main/templates/dconf.chromebook.settings
+curl -O https://raw.githubusercontent.com/$GITHUB_USER/alis-conf/main/templates/dconf.T14.settings
 
 # Configure this particular environment
 chmod +x configure.sh
